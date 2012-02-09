04b8cbd50f7f19e054817ad35d7b3bd553f2876b
will howard <will.howard@gmail.com>
2012-02-09 18:56:34 -0500

    more stuff.

--- mailStealer-daemon.py
+++ mailStealer-daemon.py
@@ -4,47 +4,41 @@ from datetime import datetime
 import imaplib
 import urllib
 import re
-import parsedatetime.parsedatetime as pdt
-import parsedatetime.parsedatetime_consts as pdc
+from dateutil import parser
 from pymongo import Connection
 
-c = pdc.Constants()
-p = pdt.Calendar(c)
-
-config = yaml.load(file('config.yaml', 'r'))
+config = yaml.load(file('/Users/williamhoward/mu-mail-visualizer/config.yaml', 'r'))
 con = imaplib.IMAP4_SSL(config['imap_add'], config['imap_port'])
 con.login(config['imap_email'], config['imap_pwd'])
 con.select(config['imap_folder'])
 typ, data = con.search(None, 'UNSEEN')
 email_data = []
 connection = Connection('localhost', 27017)
-db = connection['muEmails']
+db = connection['emailDb']
+collection = db['muEmails']
 for num in data[0].split():
     typ, e_data = con.fetch(num, '(RFC822)')
     r = re.compile('<br>', re.IGNORECASE)
     thisEmail = r.sub('\r\n', e_data[0][1])
+    email = ''
+    myDate = ''
+    client = ''
+    subject = ''
     for n in thisEmail.split('\r\n'):
-        email = ''
-        date = ''
-        client = ''
-        subject = ''
 
-        if n[:5] == 'From:':
-            email = n.split(':')[1].strip()
-        
-        if n[:5] == 'Date:':
-            print 'date: %s' % (n.replace('Date:', ''))
-            date = p.parse(n.replace('Date:', '').strip())
-        
         if n[:9] == 'X-Mailer:':
-            client = n.split(':')[1].strip()
+            client = n.replace('X-Mailer:', '').strip()
         
-        if n[:5] == 'Subject:':
-            subject = n.split(':')[1].strip()
-            
-    email_data.append({"email":email, "date":date,"client":client, "subject":subject}); 
+        if n[:8] == 'Subject:':
+            subject = n.replace('Subject:', '').strip()
 
-db.insert(email_data);
+    if subject != '':
+        email_data.append({'date':datetime.utcnow(), 'client':client, 'subject':subject}); 
+
+print email_data
+
+if len(email_data) > 0:
+    collection.insert(email_data);
 
 con.close()
 con.logout()
